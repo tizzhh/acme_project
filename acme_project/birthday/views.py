06b1e3be6755e9f33003d188ce72889cb8b9e283
Vadim Kuznetsov <darovadraste@gmail.com>
2023-11-25 00:48:48 +0300

    editing and deletion with forms.

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
 from .models import Birthday
 
 
-def birthday(request):
-    form = BirthdayForm(request.POST or None)
+def birthday(request, pk=None):
+    instance = None
+    if pk is not None:
+        instance = get_object_or_404(Birthday, pk=pk)
+    form = BirthdayForm(request.POST or None, instance=instance)
     context = {'form': form}
     if form.is_valid():
         form.save()
@@ -13,6 +16,16 @@ def birthday(request):
             form.cleaned_data['birthday']
         )
         context.update({'birthday_countdown': birthday_countdown})
+    return render(request, 'birthday/birthday.html', context)
+
+
+def delete_birthday(request, pk):
+    instance = get_object_or_404(Birthday, pk=pk)
+    form = BirthdayForm(instance=instance)
+    context = {'form': form}
+    if request.method == 'POST':
+        instance.delete()
+        return redirect('birthday:list')
     return render(request, 'birthday/birthday.html', context)
 
 
