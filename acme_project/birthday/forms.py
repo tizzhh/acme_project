81b87ddd3e452a55145a01afc9b7645209ee7d37
Vadim Kuznetsov <darovadraste@gmail.com>
2023-11-24 22:14:12 +0300

    forms with models interaction.

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,7 +1,11 @@
 from django import forms
+from .models import Birthday
 
 
-class BirthdayForm(forms.Form):
-    first_name = forms.CharField(label='Имя', max_length=20)
-    last_name = forms.CharField(label='Фамилия', required=False, help_text='Необязательное поле')
-    birthday = forms.DateField(label='Дата рождения', widget=forms.DateInput({'type': 'date'}))
+class BirthdayForm(forms.ModelForm):
+    class Meta:
+        model = Birthday
+        fields = '__all__'
+        widgets = {
+            'birthday': forms.DateInput({'type': 'date'})
+        }
