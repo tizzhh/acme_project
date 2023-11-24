df29d695ce67df144e35c1d59e5e37e52b157f0f
Vadim Kuznetsov <darovadraste@gmail.com>
2023-11-25 02:08:46 +0300

    validations.

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .validators import real_age
 
 
 class Birthday(models.Model):
@@ -6,4 +7,12 @@ class Birthday(models.Model):
     last_name = models.CharField(
         'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
     )
-    birthday = models.DateField('Дата рождения')
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint'
+            ),
+        )
