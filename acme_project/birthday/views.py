b63944cd64ef87b138bcb3e0578a0dd5a2fcfb3a
Vadim Kuznetsov <darovadraste@gmail.com>
2023-11-25 23:02:06 +0300

    cbv instead of fbv

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,44 +1,34 @@
-from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.urls import reverse_lazy
 from .forms import BirthdayForm
-from .utils import calculate_birthday_countdown
 from .models import Birthday
 
 
-def birthday(request, pk=None):
-    instance = None
-    if pk is not None:
-        instance = get_object_or_404(Birthday, pk=pk)
-    form = BirthdayForm(
-        request.POST or None,
-        files=request.FILES or None,
-        instance=instance,
-    )
-    context = {'form': form}
-    if form.is_valid():
-        form.save()
-        birthday_countdown = calculate_birthday_countdown(
-            form.cleaned_data['birthday']
-        )
-        context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context)
+class BirthdayMixin:
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
 
 
-def delete_birthday(request, pk):
-    instance = get_object_or_404(Birthday, pk=pk)
-    form = BirthdayForm(instance=instance)
-    context = {'form': form}
-    if request.method == 'POST':
-        instance.delete()
-        return redirect('birthday:list')
-    return render(request, 'birthday/birthday.html', context)
+class BirthdayFormMixin:
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
 
 
-def birthday_list(request):
-    birthdays = Birthday.objects.order_by('id')
-    paginator = Paginator(birthdays, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    return render(
-        request, 'birthday/birthday_list.html', {'page_obj': page_obj}
-    )
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = 'id'
+    paginate_by = 10
+
+
+class BirthdayDeleteView(BirthdayMixin, DeleteView):
+    # template_name = 'birthday/birthday_confirm_delete.html' # можно не указывать т.к имя шаблона подходит
+    ...
+
+
+class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
+    ...
+
+
+class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
+    ...
