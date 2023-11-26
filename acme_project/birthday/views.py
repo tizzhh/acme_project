3f1aba5f85a7767d9781ffdce24b6b6c38980558
Vadim Kuznetsov <darovadraste@gmail.com>
2023-11-26 11:40:51 +0300

    detail view, removed excessive mixins.

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,18 +1,15 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 from django.urls import reverse_lazy
+
 from .forms import BirthdayForm
 from .models import Birthday
-
-
-class BirthdayMixin:
-    model = Birthday
-    success_url = reverse_lazy('birthday:list')
-
-
-class BirthdayFormMixin:
-    form_class = BirthdayForm
-    template_name = 'birthday/birthday.html'
+from .utils import calculate_birthday_countdown
 
 
 class BirthdayListView(ListView):
@@ -21,14 +18,29 @@ class BirthdayListView(ListView):
     paginate_by = 10
 
 
-class BirthdayDeleteView(BirthdayMixin, DeleteView):
-    # template_name = 'birthday/birthday_confirm_delete.html' # можно не указывать т.к имя шаблона подходит
-    ...
+class BirthdayDeleteView(DeleteView):
+    # template_name = 'birthday/birthday_confirm_delete.html'
+    # # можно не указывать т.к имя шаблона подходит
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
 
 
-class BirthdayCreateView(BirthdayMixin, BirthdayFormMixin, CreateView):
-    ...
+class BirthdayCreateView(CreateView):
+    form_class = BirthdayForm
+    model = Birthday
 
 
-class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
-    ...
+class BirthdayUpdateView(UpdateView):
+    form_class = BirthdayForm
+    model = Birthday
+
+
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["birthday_countdown"] = calculate_birthday_countdown(
+            self.object.birthday
+        )
+        return context
